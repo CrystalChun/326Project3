3fd5609efd829aa43a16c4537b73eba1158a6246
Ubuntu <ubuntu@ip-172-31-14-109.us-west-1.compute.internal>
2017-11-14 19:29:51 +0000

    Updated to inject a bug into child process and created sample test documents

--- main.cpp
+++ main.cpp
@@ -6,77 +6,74 @@
 #include <fstream>
 using namespace std;
 
-string getDoc();
+void replaceString(string, string, string);
+void write(string);
+string getDocument();
+
 int main(int argc, const char* argv[]) {
-  bool inProgram = true;
-  ifstream filestream;
-     
-  while(inProgram) {
-    int count = 0;
+    bool inProgram = true;
     
-    string line;
-    string str = "";
-    filestream.open("document.txt");
-    
-    // Reads every line in the document
-    while(getline(filestream, line)) {
-      str += line;
+       
+    while(inProgram) {
+                
+        string str = getDocument();
+        
+        cout << "\nDocument: " << str << endl << endl;
+
+        cout << "Enter the string to replace: ";
+        string replace;
+        cin >> replace;
+        cout << endl;
+        
+        if(replace.compare("!wq") == 0) {
+            cout << "Good bye." << endl;
+            break;
+        }
+        
+        cout << "Enter the string to replace with: ";
+        string replaceWith;
+        cin >> replaceWith;
+        cout << endl;
+
+        int pid = fork();
+        wait(NULL);
+
+        
+        if(pid == 0) {
+            // In child process
+            replaceString(str, replace, replaceWith);      
+            
+            inProgram = false;
+        } else if (pid > 0 ) { // In parent process
+        } else {
+            cout << "Error forking" << endl;
+        }
     }
-    filestream.close(); 
-
-    cout << "Document: " << str << endl;
-
-    cout << "Enter the string to replace: ";
-    string replace;
-    cin >> replace;
-    cout << endl;
-    
-    if(replace.compare("!wq") == 0) {
-      cout << "Good bye." << endl;
-      break;
-    }
-    
-    cout << "Enter the string to replace with: ";
-    string replaceWith;
-    cin >> replaceWith;
-    cout << endl;
-
-    int pid = fork();
-    wait(NULL);
-
-    
-    if(pid == 0) {
-      // In child process
-      
-      replace(str, replace, replaceWith);      
-      
-      inProgram = false;
-    } else if (pid > 0 ) { // In parent process
-    } else {
-      cout << "Error forking" << endl;
-    }
-  }
-  return 0;
+    return 0;
 }
 
 /** 
  * Replaces the specified string with the given replaceWith
  * string in the specified document.
  */
-void replace(string document, string replace, string replaceWith) {
+void replaceString(string document, string replace, string replaceWith) {
     size_t loc = document.find(replace);
-      
-      // Replace the string with the string to replace with if it still exists
-      while(loc != string::npos) {
+    int count = 0; // The number of times the string was replaced.
+ 
+    // Replace the string with the string to replace with if it still exists
+    while(loc != string::npos) {
         document.replace(loc, replace.length(), replaceWith);
         count ++;
         loc = document.find(replace);
-      }
+    }
+  
+    while(count == 0) {
+      cout << "." << endl;
+    }
+    cout << "Replaced " << replace << " with " 
+      << replaceWith << " " << count << " times." << endl;
 
-      cout << "Replaced " << replace << " with " 
-        << replaceWith << " " << count << " times." << endl;
-
-      write(document);
+    write(document);
 }
 /**
  * Writes the specified document to file.
@@ -88,4 +85,22 @@ void write(string document) {
     
     writeStream << document;
     writeStream.close();
+}
+
+/**
+ * Reads the document from file and returns a
+ * string of its contents.
+ */
+string getDocument() {
+    ifstream filestream;
+    string line; 
+    string str = "";
+    filestream.open("document.txt");
+    
+    // Reads every line in the document
+    while(getline(filestream, line)) {
+      str += line;
+    }
+    filestream.close(); 
+    return str;
 }
